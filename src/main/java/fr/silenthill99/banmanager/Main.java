1c2807d0e9d3dd2407433ba610d4c7067f2100d1
BuildTools <unconfigured@null.spigotmc.org>
2024-06-20 22:14:42 +0200

    Base du projet

--- src/main/java/fr/silenthill99/banmanager/Main.java
+++ src/main/java/fr/silenthill99/banmanager/Main.java
@@ -4,10 +4,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    private static Main instance;
+
+    public static Main getInstance() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        getLogger().info("Le plugin est opérationnel !");
     }
 
     @Override
